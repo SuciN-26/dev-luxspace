c0fe58210f0892aa61dcda4c0ec159b945893504
sucinopikirana <sucinopikirana@gmail.com>
2024-07-04 15:21:21 +0700

    initialize commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'src/assets/css/app.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1 className="text-3xl font-bold underline bg-green-500 hover:bg-green-700">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
